73c10fd485c69f5ed23652b68be68ae3f05abf9a
José Vasconcelos <zevasconcelos6@gmail.com>
2023-07-02 23:08:22 +0100

    .

--- exercises/ex9.exs
+++ exercises/ex9.exs
@@ -1,8 +1,8 @@
 defmodule Ex9 do
   def replicate(n,_) when n<1,do: []
-  def replicate(1,x),do: [x]
+  #def replicate(1,x),do: [x]
   def replicate(n,x),do: [x|replicate(n-1,x)]
 end
 
-IO.inspect(Ex9.replicate(0, 10))
+#IO.inspect(Ex9.replicate(1, 10))
 #nao esta a funcionar bem
